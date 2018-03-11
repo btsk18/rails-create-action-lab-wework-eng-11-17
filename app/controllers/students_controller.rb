18f288a3035923365e313112741259efe8bf07ab
Brandon Leeds <brandonleeds@gmail.com>
2018-03-11 23:46:29 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     Student.create(params)
+    redirect_to students_path
   end
 
 end
